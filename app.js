2c5224989eac57f2a4800545af029e1054580d95
Jessie Samuelson <jessie.samuelson@gmail.com>
2017-02-04 15:23:24 -0500

    adds user authentication using AUTH0

--- app.js
+++ app.js
@@ -4,19 +4,51 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var dotenv = require('dotenv');
+var dotenv = require('dotenv')
+
+// Passport is the most popular Node Js authentication library
+var passport = require('passport');
+// We are including the Auth0 authentication strategy for Passport
+var Auth0Strategy = require('passport-auth0');
 
 // We are using the dotenv library to load our environmental variables from the .env file.
 dotenv.load();
 
 var routes = require('./routes/index');
 
+console.log('process.env.AUTH0_CLIENT_ID ~~>', process.env.AUTH0_CLIENT_ID);
+
+// This will configure Passport to use Auth0
+var strategy = new Auth0Strategy({
+  domain:       process.env.AUTH0_DOMAIN,
+  clientID:     process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL:  'http://localhost:4000/callback'
+}, function(accessToken, refreshToken, extraParams, profile, done) {
+  // accessToken is the token to call Auth0 API (not needed in the most cases)
+  // extraParams.id_token has the JSON Web Token
+  // profile has all the information from the user
+  return done(null, profile);
+});
+
+
+// Here we are adding the Auth0 Strategy to our passport framework
+passport.use(strategy);
+
+// The searlize and deserialize user methods will allow us to get the user data once they are logged in.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 // This line of code instantiates the Express JS framework. We assign it to a variable called app, and will add our configruation to this variable.
 var app = express();
 
 // The .set method allows us to configure various options with the Express framework. Here we are setting our views directory as well as telling Express that our templating engine will be Jade. More on that soon.
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 app.set('view engine', 'pug');
 
 // The .use method is similar to the .set method, where it allows us to set further configurations. The .use method also acts as a chain of events that will take place once a request hits our Node Js application. First we'll log the request data, parse any incoming data, and so on.
@@ -30,6 +62,10 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));
+// We are also adding passport to our middleware flow
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Here we are going to use add our routes in a use statement which will link the routes we defined to our app.
@@ -42,10 +78,15 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+
 // If our applicatione encounters an error, we'll display the error and stacktrace accordingly.
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send(err);
+  //res.send(err)
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
 });
 
 // Finally, we'll choose to have our app listen on port 3000. This means that once we launch our app, we'll be able to navigate to localhost:3000 and see our app in action. You are free to choose any port you want, so 8080, or 80, or really any number will work. The reason 3000 is typically used is because it's the lowest port number that can be used without requiring elevated privileges on Mac/Linux systems.
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,57 @@
-// Again we are importing the libraries we are going to use
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+var request = require('request');
 
-// On our router variable, we'll be able to include various methods. For our app we'll only make use of GET requests, so the method router.get will handle that interaction. This method takes a string as its first parameter and that is the url path, so for the first route we are just giving it '/', which means the default route. Next we are defining a Node Js callback function, that takes three parameters, a request (req), a response (res), and an optional next (next) parameter. Finally, in our callback function, we are just send the message "You are on the homepage".
-// router.get('/', function(req, res, next) {
-//   res.send('You are on the homepage');
-// });
+// We are going to want to share some data between our server and UI, so we'll be sure to pass that data in an env variable.
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL: 'http://localhost:4000/callback'
+};
 
 router.get('/', function(req, res, next) {
-   res.render('index', {})
+  // Now, rather then just sending the text "You are on the homepage", we are going to actually render the view we created using the res.render method. The second argument will allow us to pass in data from the backend to our view dynamically.
+  res.render('index', { env: env });
 });
 
-
-// We are going to do the same thing for the remaining routes.
 router.get('/login',function(req, res){
-  res.send('You are on the login page');
+  // Same thing for the login page.
+  res.render('login', { env: env });
 });
 
 router.get('/logout', function(req, res){
-  res.send('You are on the logout page');
+  // For the logout page, we don't need to render a page, we just want the user to be logged out when they hit this page. We'll use the ExpressJS built in logout method, and then we'll redirect the user back to the homepage.
+  req.logout();
+  res.redirect('/');
 });
 
-router.get('/polls', function(req, res){
-  res.send('You are on the polls page');
+router.get('/polls', ensureLoggedIn, function(req, res){
+  // You may have noticed that we included two new require files, one of them being request. Request allows us to easily make HTTP requests. In our instance here, we are using the Huffington Post's API to pull the latest election results, and then we're sending that data to our polls view.
+  // The second require was the connect-ensure-loggedin library, and from here we just required a method called ensureLoggedIn, which will check and see if the current user is logged in before rendering the page. If they are not, they will be redirected to the login page. We are doing this in a middleware pattern, we first call the ensureLoggedIn method, wait for the result of that action, and finally execute our /polls controller.
+  request('http://elections.huffingtonpost.com/pollster/api/charts.json?topic=2016-president', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var polls = JSON.parse(body);
+      // For this view, we are not only sending our environmental information, but the polls and user information as well.
+      res.render('polls', {env: env, user: req.user, polls: polls});
+    } else {
+      res.render('error');
+    }
+  })
 })
 
-router.get('/user', function(req, res, next) {
-  res.send('You are on the user page');
+router.get('/user', ensureLoggedIn, function(req, res, next) {
+  // Same thing for our
+  res.render('user', { env: env, user: req.user });
 });
 
-// Finally, we export this module so that we can import it in our app.js file and gain access to the routes we defined.
+// We are also going to implement the callback route which will redirect the logged in user to the polls page if authentication succeeds.
+router.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/polls');
+  }
+);
+
 module.exports = router;
